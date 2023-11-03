55b192448244b259437920bd1670ac1e9c764e13
Sean Gloumeau <sajgloumeau@protonmail.com>
2023-11-03 11:21:39 +0100

    Added getter methods for trips and quads

--- src/kg.py
+++ src/kg.py
@@ -1,5 +1,6 @@
 from collections.abc import Mapping, Sequence
-from typing import Any, Tuple
+from typing import Any, List, Set, Tuple
+import frozendict
 
 import networkx as nx
 
@@ -19,7 +20,8 @@ class KnowledgeGraph:
             self.add_attribute_triples(attr_trips)
         if rel_quads:
             self.add_relation_quadruples(rel_quads)
-    
+
+
     # Methods for KG management
     
     def add_attribute_triples(self, attr_trips: Sequence[AttributeTriple]) -> None:
@@ -38,3 +40,35 @@ class KnowledgeGraph:
     def add_relation_quadruple(self, rel_quad: RelationQuadruple) -> None:
         self.mdg.add_edge(rel_quad[0], rel_quad[2], key=rel_quad[1], **rel_quad[3])
         self.relations.add(rel_quad[1])
+
+
+    # Methods for accessing KG data
+
+    def entities(self) -> Set[Entity]:
+        return set(self.mdg.nodes())
+    
+    def attribute_triples(self, entity: Entity = None) -> Set[AttributeTriple]:
+        if entity:
+            if entity not in self.entities():
+                raise LookupError(f"Entity {entity} not in knowledge graph")
+            nodes = [(entity, self.mdg.nodes(data=True)[entity])]
+        else:
+            nodes = self.mdg.nodes(data=True)
+        triples = set()
+        for node in nodes:
+            triples.update(set([(node[0], attr, val) for attr, val in node[1].items()]))
+        return triples
+
+    def relation_quadruples(self, head_entity: Entity = None, 
+            tail_entity: Entity = None) -> List[RelationQuadruple]:
+        if head_entity and tail_entity:
+            all_quads_from_head = list(self.mdg.edges(head_entity, data=True, keys=True))
+            quads = [quad for quad in all_quads_from_head if quad[1] == tail_entity]
+        elif head_entity:
+            quads = list(self.mdg.edges(head_entity, data=True, keys=True))
+        else:
+            quads = list(self.mdg.edges(data=True, keys=True))
+        quads = [(src_entity, relation, dest_entity, attributes) 
+                 for src_entity, dest_entity, relation, attributes in quads]
+        return quads
+    
